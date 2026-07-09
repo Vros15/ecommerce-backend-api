51b1d7b7e9c6cca3ed889995ffbabd5e2dc9dd88
Victor Rosario <vros15@yahoo.com>
2026-07-09 17:12:51 -0400

    feat: add GET route for retrieving customer cart for endpoint: GET /api/carts/:customer

--- routes/cartsRouter.js
+++ routes/cartsRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {createOneCart} = require("../controllers/cartsController");
+const {createOneCart, getCart} = require("../controllers/cartsController");
 
 // Route to create a new cart
 //endpoint: POST /api/carts
 router.post("/", createOneCart);
+
+// Route to get a cart by customer
+//endpoint: GET /api/carts/:customer
+router.get("/:customer", getCart);
 
 module.exports = router;
