5561dc00283006ed2c0cc06f401c93d9d94ea419
Victor Rosario <vros15@yahoo.com>
2026-07-09 10:08:20 -0400

    feat: add GET route for retrieving all products for endpoint GET /api/products

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct } = require('../controllers/productsController');
+const { createProduct,getAllProducts } = require('../controllers/productsController');
 
 // Route to create a new product
 //endpoint: POST /api/products
 router.post("/", createProduct);
+// Route to get all products
+//endpoint: GET /api/products
+router.get("/", getAllProducts);
 
 module.exports = router;
