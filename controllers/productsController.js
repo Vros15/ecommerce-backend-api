ade5fd2e87c069503e1171bbf219f0fc57edceef
Victor Rosario <vros15@yahoo.com>
2026-07-09 10:07:30 -0400

    feat: implement getAllProducts controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -12,6 +12,16 @@ const createProduct = async (req, res) => {
     }
 }
 
+const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json({ message: "Products retrieved successfully", products });
+    } catch (error) {
+        res.status(500).json({ message: "Error retrieving products", error });
+    }
+}
+
 module.exports = {
     createProduct,
+    getAllProducts,
 };
