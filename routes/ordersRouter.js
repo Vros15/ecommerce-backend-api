ba26c8c056bc911ace05bc71f5d959c51052559e
Victor Rosario <vros15@yahoo.com>
2026-07-12 11:38:24 -0400

    feat: add GET route for retrieving all orders for endpoint: GET /orders/

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -3,16 +3,16 @@
 // Import the Express router and the orders controller
 const express = require("express");
 const router = express.Router();
-const {createOrderFromCart} = require("../controllers/ordersController");
+const {createOrderFromCart,getAllOrders} = require("../controllers/ordersController");
 
 //POST create a new order
 //endpoint: POST /order/:customer
 router.post("/:customer", createOrderFromCart);
 
 //GET all orders & Filter by status
+router.get("/", getAllOrders);
 
 //GET a single order by ID
-
 
 //PUT update status of an order by ID
 
